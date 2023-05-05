41c8b1b43384db042dd49664ba7c27ae03c2395e
beni <mamont4444@live.ru>
2023-05-05 22:40:10 +0300

    created express

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Beni Best')
+const express = require('express')
+const app = express();
+const PORT = (process.env.NODE_ENV === 'development' ? 2000 : 3000);
+
+
+app.get('/', (req, res) => {
+
+    console.log(req)
+    console.log(res)
+
+    res.send('hello world')
+})
+
+app.get('/json', (req, res) => {
+    
+    res.json({ "name": "Beni" })
+})
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`app listening on port ${PORT}`)
+})
